9c992df722a0df3db7f4485a35e222e3ea9c99c1
gitongah <mbaabugitonga@gmail.com>
2024-05-22 20:05:43 -0700

    worked on package.json file added the scripts to run the application

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,28 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// logic to a method that accepts some content and adds it to the database
+export const postDb = async (content) => {
+  console.log('Post to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ tate: content});
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET all from the database');
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+
+}
 
 initdb();
